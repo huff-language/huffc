c3a68c0dc8f8856156204fa0d8120246680e3e38
JetDeveloping <jetjadeja@gmail.com>
2021-07-28 00:03:04 -0700

    ✨ Fixed compile command

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
 import { program } from "commander";
 import compiler from "./compiler";
 
-program.option("-a, --args <args>", "arguments", "");
+console.log("");
+
+program
+  .option("-a, --args <args>", "arguments", "")
+  .option("-d, --dir <dir>", "default directory for contracts", "./");
 
 program.parse(process.argv);
 const options = program.opts();
@@ -9,14 +13,28 @@ const options = program.opts();
 const command = process.argv[2];
 
 if (command === "compile") {
-  const file = process.argv[3];
-  const path = file.split("/");
+  const errors = [];
 
-  console.log(`Compiling ${path[path.length - 1]}`, "\n");
-  const code = `0x${compiler(process.argv[3], "./", options.args, {})}`;
+  for (let i = 3; i < process.argv.length; i++) {
+    const file = process.argv[i];
+    const path = file.split("/");
 
-  console.log("\x1b[32m", "Compiled Sucessfully", "\x1b[0m");
-  console.log(code, "\n");
+    if (file.startsWith("--")) continue;
+
+    try {
+      console.log("-----------------------------");
+      console.log(`\x1b[1mCompiling ${path[path.length - 1]}`, "\n\x1b[0m");
+      const code = `0x${compiler(process.argv[i], options.dir, options.args, {})}`;
+      console.log("\x1b[32m", "Compiled Sucessfully", "\x1b[0m");
+      console.log(code, "\n");
+    } catch (e) {
+      errors.push(e);
+    }
+  }
+
+  for (let i = 0; i < errors.length; i++) {
+    console.log(errors[i]);
+  }
 }
 
 // if () {
