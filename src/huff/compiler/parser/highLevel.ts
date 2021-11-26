88fee82140810cf4f1e359203092a89124ab5626
JetDeveloping <jetjadeja@gmail.com>
2021-11-25 16:21:40 -1000

    Added high level file content reader

--- src/huff/compiler/parser/highLevel.ts
+++ src/huff/compiler/parser/highLevel.ts
@@ -11,13 +11,26 @@ import { removeComments } from "../../utils/langauge/parsing";
  * @returns An array of maps, containing the filename and raw data
  * and a string containing the raw data of all files combined
  */
-const getFileContents = (
-  path: string
-): { fileData: { path: string; rawInput: string }[]; raw: string } => {
-  // Read the data from the file.
-  const fileString = removeComments(readFile(path));
+const getFileContents = (path: string): { filename: string; data: string }[] => {
+  // Array of filedata.
+  const includes: { filename: string; data: string }[] = [];
 
-  return { fileData: [], raw: "" };
+  // Local function that reads a file and adds it to the includes map.
+  // We instantiate this locally because we need to use it recursively to read all imports.
+  const readFileData = (path: string): { filename: string; data: string }[] => {
+    // Read the data from the file.
+    const fileString = removeComments(readFile(path));
+    let includes: { filename: string; data: string }[] = [{ filename: path, data: fileString }];
+
+    // Read the file data of all the imported files.
+    getImports(fileString).forEach((importPath) => {
+      includes = [...includes, ...readFileData(importPath)];
+    });
+
+    return includes;
+  };
+
+  return readFileData(path);
 };
 
 /**
@@ -34,5 +47,6 @@ const getImports = (raw: string): string[] => {
     // Remove the import statement.
     raw = raw.slice(nextImport[0].length);
   }
+
   return imports;
 };
