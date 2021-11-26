e7fd75607c36198022cac2e71d5d29f9893162ce
JetDeveloping <jetjadeja@gmail.com>
2021-11-25 21:19:25 -1000

    Added high-level definition retriever

--- src/huff/compiler/parser/highLevel.ts
+++ src/huff/compiler/parser/highLevel.ts
@@ -2,6 +2,55 @@
 import { readFile } from "../../utils/helpers/data/files";
 import { HIGH_LEVEL } from "../../utils/langauge/syntax";
 import { removeComments } from "../../utils/langauge/parsing";
+import { isEndOfData } from "../../utils/helpers/regex";
+
+/**
+ * Given file data, return three arrays containing the macros, constants, and tables.
+ */
+const getHighLevelDefinitions = (
+  data: { filename: string; data: string }[]
+): {
+  macros: { name: string; raw: string }[];
+  constants: { name: string; value: string }[];
+  tables: { name: string; raw: string }[];
+} => {
+  // Arrays
+  const macros: { name: string; raw: string }[] = [];
+  const constants: { name: string; value: string }[] = [];
+  const tables: { name: string; raw: string }[] = [];
+
+  // Parse the data
+  data.forEach((file) => {
+    // Store the current file data.
+    let input = file.data;
+
+    // Parse the file data.
+    while (!isEndOfData(input)) {
+      // Check if we are parsing a macro definition.
+      if (HIGH_LEVEL.MACRO.test(input)) {
+        // Parse macro definition.
+        const macro = input.match(HIGH_LEVEL.MACRO);
+
+        // Add the macro to the array.
+        macros.push({ name: macro[2], raw: macro[0] });
+
+        // Slice the input
+        input = input.slice(macro[0].length);
+      } else if (HIGH_LEVEL.CONSTANT.test(input)) {
+        // Parse constant definition.
+        const constant = input.match(HIGH_LEVEL.CONSTANT);
+
+        // Add the constant to the array.
+        constants.push({ name: constant[2], value: constant[3] });
+
+        // Slice the input
+        input = input.slice(constant[0].length);
+      }
+    }
+  });
+
+  return { macros, constants, tables };
+};
 
 /**
  *
@@ -44,6 +93,9 @@ const getImports = (raw: string): string[] => {
 
     // Remove the import statement.
     raw = raw.slice(nextImport[0].length);
+
+    // Search for the next import statement.
+    nextImport = raw.match(HIGH_LEVEL.IMPORT);
   }
 
   return imports;
