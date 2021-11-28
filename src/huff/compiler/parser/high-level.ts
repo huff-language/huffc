01bdaae8360f7f33cd0b6de6298cb4a13a9e4647
JetDeveloping <jetjadeja@gmail.com>
2021-11-28 10:33:39 -1000

    Merged definitions and high-level

--- src/huff/compiler/parser/high-level.ts
+++ src/huff/compiler/parser/high-level.ts
@@ -1,40 +1,36 @@
-/* Utils */
-import { readFile } from "../../utils/helpers/data/files";
-import { HIGH_LEVEL } from "../../utils/langauge/syntax";
-import { removeComments } from "../../utils/langauge/parsing";
+/* Helpers Utils */
 import { isEndOfData } from "../../utils/helpers/regex";
+import { readFile } from "../../utils/helpers/data/files";
+
+/* Language Utils */
+import { removeComments } from "../../utils/langauge/parsing";
+import { HIGH_LEVEL, MACRO_CODE } from "../../utils/langauge/syntax";
+
+/* Parser Utils */
+import { Definitions } from "../utils/enums";
+
+/* Data Utils */
+import {
+  convertNumberToBytes,
+  convertBytesToNumber,
+  findLowest,
+} from "../../utils/helpers/data/bytes";
 
 /**
  * @param path File path to be parsed.
  * @returns Three arrays, containing the macros, constants, and tables
  * from the Huff file and from all files that it imports.
  */
-const parseFile = (
-  path: string
-): [
-  {
-    macros: { [name: string]: { args: number; body: string } };
-    constants: { [name: string]: string };
-    tables: { name: string; raw: string }[];
-  },
-  { macros: string[]; constants: string[] }
-] => {
+const parseFile = (path: string): [Definitions, { macros: string[]; constants: string[] }] => {
   return getHighLevelDefinitions(getFileContents(path));
 };
 
 /**
  * Given file data, return three maps containing the macros, constants, and tables.
  */
-const getHighLevelDefinitions = (
+export const getHighLevelDefinitions = (
   data: { filename: string; data: string }[]
-): [
-  {
-    macros: { [name: string]: { args: number; body: string } };
-    constants: { [name: string]: string };
-    tables: { name: string; raw: string }[];
-  },
-  { macros: string[]; constants: string[] }
-] => {
+): [Definitions, { macros: string[]; constants: string[] }] => {
   // Dictionaries
   const macros: { [name: string]: { args: number; body: string } } = {};
   const constants: { [name: string]: string } = {};
@@ -59,6 +55,9 @@ const getHighLevelDefinitions = (
       if (HIGH_LEVEL.MACRO.test(input)) {
         // Parse macro definition.
         const macro = input.match(HIGH_LEVEL.MACRO);
+
+        console.log(macro[3]);
+        console.log(parseArgs(macro[3]));
 
         // macros[name] = {body, takes}
         macros[macro[2]] = { body: macro[7], args: parseInt(macro[3]) };
@@ -102,6 +101,102 @@ const getHighLevelDefinitions = (
   ];
 
   //return { macros, constants, tables };
+};
+
+/**
+ * Return arrays of used macros, tables, and constants.
+ * @param macros an array of macros to search.
+ * @param data List of all macros, tables, and constants.
+ */
+export const getUsedDefinitions = (
+  macrosToParse: string[],
+  data: Definitions
+): {
+  macros: string[];
+  constants: string[];
+  tables: string[];
+} => {
+  // Arrays of functions to search for.
+  const macros: string[] = [];
+  const constants: string[] = [];
+
+  // Iterate over the inputted array of macros.
+  macrosToParse.forEach((name) => {
+    // Retrieve macro from map.
+    const macro = data.macros[name];
+
+    // If the macro doesn't exist, move to the next macro.
+    if (!macro) return;
+
+    // Get the body of the macro.
+    let body = macro.body;
+
+    // Iterate over the macro body.
+    while (!isEndOfData(body)) {
+      // If the next call is a constant call.
+      if (body.match(MACRO_CODE.CONSTANT_CALL)) {
+        const constantName = body.match(MACRO_CODE.CONSTANT_CALL)[0].replace(" ", "");
+
+        if (data.constants[constantName]) {
+          constants.push(constantName);
+        } else {
+          throw `${constantName} is not defined`;
+        }
+
+        body = body.slice(body.match(MACRO_CODE.CONSTANT_CALL)[0].length);
+      } else {
+        body = body.slice(1);
+      }
+    }
+  });
+
+  return { macros, constants, tables: [] };
+};
+
+/**
+ * Assign constants that use the builtin FREE_STORAGE_POINTER(
+ * @param constants Maps the name of constants to their values
+ * @param order The order that the constants were declared in
+ */
+export const setStoragePointerConstants = (
+  constants: { [name: string]: string },
+  order: string[]
+) => {
+  const usedPointers: number[] = [];
+
+  // Iterate over the array of constants.
+  order.forEach((name) => {
+    if (name.endsWith("_STORAGE_POINTER")) {
+      const value = constants[name];
+
+      // If the value is a hex literal.
+      if (value.startsWith("0x")) {
+        /* 
+        If the pointer is already used, throw an error.
+        In order to safely circumvent this, all constant-defined pointers must be defined before
+        pointers that use FREE_STORAGE_POINTER.
+        */
+        if (usedPointers.includes(convertBytesToNumber(value))) {
+          throw `Constant ${name} uses already existing pointer`;
+        }
+
+        // Add the pointer to the list of used pointers.
+        usedPointers.push(convertBytesToNumber(value));
+      }
+
+      // The value calls FREE_STORAGE_POINTER.
+      else if (value == "FREE_STORAGE_POINTER()") {
+        // Find the lowest available pointer value.
+        const pointer = findLowest(0, usedPointers);
+
+        // Add the pointer to the list of used pointers.
+        usedPointers.push(pointer);
+
+        // Set the constant to the pointer value.
+        constants[name] = convertNumberToBytes(pointer);
+      }
+    }
+  });
 };
 
 /**
@@ -152,6 +247,14 @@ export const getImports = (raw: string): string[] => {
   }
 
   return imports;
+};
+
+/**
+ * Parse an arguments list and convert it to an array
+
+ */
+const parseArgs = (argString) => {
+  return argString.replace(" ", "").split(",");
 };
 
 // Default export should be parseFile.
