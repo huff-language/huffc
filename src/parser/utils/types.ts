b1817f42251801d31aed09d4dab5b2fff6ebcc63
JetDeveloping <jetjadeja@gmail.com>
2021-12-26 01:24:50 -0800

    Add operation types

--- src/parser/utils/types.ts
+++ src/parser/utils/types.ts
@@ -4,8 +4,31 @@ export type Definitions = {
   defintions: string[];
 };
 
+/* Type Representing an Operation */
+export type Operation = {
+  type: OperationType;
+  value: string;
+  args: any[];
+  ops?: Operation[];
+};
+
+/* Operation Type */
+export enum OperationType {
+  OPCODE = "OPCODE",
+  PUSH = "PUSH",
+  JUMPDEST = "JUMPDEST",
+  PUSH_JUMP_LABEL = "PUSH_JUMP_LABEL",
+
+  MACRO_CALL = "MACRO_CALL",
+  CONSTANT_CALL = "CONSTANT_CALL",
+  ARG_CALL = "ARG_CALL",
+
+  CODESIZE = "CODESIZE",
+  TABLE_START_POSITION = "TABLE_START_POSITION",
+}
+
 /* Top Level Context */
-enum Context {
+export enum Context {
   NONE = 0,
   MACRO = 1,
 }
