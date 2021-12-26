958b6f02b25ee60c0600b6a390a7004b1078f644
JetDeveloping <jetjadeja@gmail.com>
2021-12-26 00:05:06 -0800

    Implement jump table parser method

--- src/parser/tables.ts
+++ src/parser/tables.ts
@@ -1,4 +1,4 @@
-import { JUMP_TABLES } from "./syntax/defintions";
+import { HIGH_LEVEL, JUMP_TABLES } from "./syntax/defintions";
 import { removeSpacesAndLines } from "./utils/regex";
 
 /**
@@ -16,4 +16,29 @@ export const parseCodeTable = (body: string): { table: string; size: number } =>
 
   // Return the table data.
   return { table, size: table.length / 2 };
+};
+
+/**
+ * Parse a jumptable definition
+ * @param body The raw string representing the body of the table.
+ */
+export const parseJumpTable = (
+  body: string,
+  compressed = false
+): { jumps: string[]; size: number } => {
+  // Parse the body of the table
+  const jumps = body.match(JUMP_TABLES.JUMPS).map((jump) => {
+    return removeSpacesAndLines(jump);
+  });
+
+  // Calculate the size of the table.
+  let size;
+  if (compressed) size = jumps.length * 0x02;
+  else size = jumps.length * 0x20;
+
+  // Return the array of jumps and the size of the table.
+  return {
+    jumps,
+    size,
+  };
 };
