94c923a691e0993760f776a27473e79dd5036aa0
JetDeveloping <jetjadeja@gmail.com>
2021-12-26 23:16:47 -0800

    Complete macro processor

--- src/compiler/processor.ts
+++ src/compiler/processor.ts
@@ -1,9 +1,20 @@
 import opcodes from "../evm/opcodes";
 import { Definitions, Operation, OperationType } from "../parser/utils/types";
-import { formatEvenBytes, toHex } from "../utils/bytes";
+import { formatEvenBytes, padNBytes, toHex } from "../utils/bytes";
 
+/**
+ * Process a macro, generating semi-complete EVM bytecode.
+ * @param name The name of the macro.
+ * @param bytecodeOffset The offset of the macro's bytecode.
+ * @param args The arguments passed into the array.
+ * @param macros Maps all macros to their raw text and arguments.
+ * @param constants Maps all constants to their values.
+ * @param jumptables Maps all jump tables to their jump values.
+ * @returns Semi-complete EVM bytecode.
+ */
 export const processMacro = (
   name: string,
+  bytecodeOffset: number,
   args: string[],
   macros: Definitions["data"],
   constants: Definitions["data"],
@@ -23,7 +34,7 @@ export const processMacro = (
   const jumptable = [];
   let jumpindices: { [name: string]: number } = {};
   let tableInstances: any[] = [];
-  let offset: number = 0;
+  let offset: number = bytecodeOffset;
 
   // Store a copy of the body and args.
   const codes = macro.data.map((operation: Operation, index: number) => {
@@ -31,7 +42,14 @@ export const processMacro = (
       // We're parsing a macro call.
       case OperationType.MACRO_CALL: {
         // Process the macro call.
-        const result = processMacro(operation.value, operation.args, macros, constants, jumptables);
+        const result = processMacro(
+          operation.value,
+          offset,
+          operation.args,
+          macros,
+          constants,
+          jumptables
+        );
 
         // Add the result to local variables.
         tableInstances = [...tableInstances, ...result.tableInstances];
@@ -96,6 +114,7 @@ export const processMacro = (
         // Calculate the code of the macro.
         const code = processMacro(
           operation.value,
+          offset,
           operation.args,
           macros,
           constants,
@@ -154,6 +173,71 @@ export const processMacro = (
     }
   });
 
+  // Store the current index.
+  let currentIndex = 0;
+
+  // Loop through the code definitions.
+  const indices = codes.map((bytecode) => {
+    // Update the current index.
+    currentIndex += bytecode.length / 2;
+
+    // Return the index.
+    return currentIndex;
+  });
+
+  // Store an array of unmatched jumps.
+  const unmatchedJumps = [];
+
+  // Get the absolute bytecode index for each jump label.
+  const data = codes.reduce((accumulator, bytecode, index) => {
+    // Store a formatted version of the bytecode.
+    let formattedBytecode = bytecode;
+
+    // Check a jump table exists at this index.
+    if (jumptable[index]) {
+      // Store the jumps.
+      const jumps = jumptable[index];
+
+      // Iterate over the jumps at this index.
+      for (const { label: jumplabel, bytecodeIndex } of jumps) {
+        // If the jumplabel is defined:
+        if (jumpindices.hasOwnProperty(jumplabel)) {
+          // Get the absolute bytecode index and pad the value (1 byte).
+          const jumpvalue = padNBytes(toHex(jumpindices[jumplabel]), 2);
+
+          // Slice the bytecode to get the code before and after the jump.
+          const before = formattedBytecode.slice(0, bytecodeIndex + 2);
+          const after = formattedBytecode.slice(bytecodeIndex + 6);
+
+          // Ensure that the jump value is set with a placeholder.
+          if (formattedBytecode.slice(bytecodeIndex + 2, bytecodeIndex + 6) !== "xxxx") {
+            throw new Error(
+              `Expected indicies ${bytecodeIndex + 2} to ${
+                bytecodeIndex + 6
+              } to be jump location, of
+                          ${formattedBytecode}`
+            );
+            // Insert the jump value.
+            formattedBytecode = `${before}${jumpvalue}${after}`;
+          }
+          // If the jumplabel has not been definied:
+          else {
+            // Store the offset.
+            const jumpOffset = (indices[index] - bytecodeOffset) * 2;
+
+            // Push the jump value to the unmatched jumps array.
+            unmatchedJumps.push({ label: jumplabel, bytecodeIndex: jumpOffset + bytecodeIndex });
+          }
+        }
+      }
+    }
+
+    // Return the new bytecode.
+    return accumulator + formattedBytecode;
+  }, "");
+
+  // Add the initial index to the start of the array.
+
   // Return the result.
-  return { bytecode: codes.join(""), tableInstances, jumptable, jumpindices };
+  return { bytecode: codes.join(""), unmatchedJumps, tableInstances, jumptable, jumpindices };
 };
