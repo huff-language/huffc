4e0c63db3d7367e7c6f1d47a6670f9e5907057b5
JetDeveloping <jetjadeja@gmail.com>
2021-12-28 19:23:33 -0800

    Add compiler file

---
+++ src/index.ts
@@ -0,0 +1,68 @@
+import { padNBytes, toHex } from "./utils/bytes";
+import { compileMacro } from "./compiler/compiler";
+import { parseFile, setStoragePointerConstants } from "./parser/high-level";
+import { ethers } from "../tests/huff/node_modules/ethers/lib";
+
+/**
+ * Compile a Huff file.
+ * @param filePath The path to the file.
+ * @param args An array containing the arguments to the macro.
+ * @returns The compiled bytecode.
+ */
+const compile = (filePath: string, args: [{ type: string; value: string }]) => {
+  // Parse the file and generate definitions.
+  const { macros, constants, functions, tables } = parseFile(filePath);
+
+  // Set storage pointer constants.
+  constants.data = setStoragePointerConstants(["CONSTRUCTOR", "MAIN"], macros.data, constants);
+
+  // Compile the macros.
+  const mainBytecode = compileMacro("MAIN", [], macros.data, constants.data, tables.data);
+  const constructorBytecode = compileMacro(
+    "CONSTRUCTOR",
+    [],
+    macros.data,
+    constants.data,
+    tables.data
+  );
+
+  // Store the sizes of the bytecode.
+  const contractLength = mainBytecode.length / 2;
+  const constructorLength = constructorBytecode.length / 2;
+
+  // Convert the sizes and offset to bytes.
+  const contractSize = padNBytes(toHex(contractLength), 2);
+  const contractCodeOffset = padNBytes(toHex(13 + constructorLength), 2);
+
+  // push2(contract size) dup1 push2(offset to code) push1(0) codecopy push1(0) return
+  const bootstrapCode = `61${contractSize}8061${contractCodeOffset}6000396000f3`;
+  const constructorCode = `${constructorBytecode}${bootstrapCode}`;
+  const deployedBytecode = `${constructorCode}${mainBytecode}`;
+
+  // If there are passed args, encode them.
+  encodeArgs(args);
+
+  // Return the bytecode.
+  return deployedBytecode;
+};
+
+/**
+ * Encode arguments.
+ * @param args The arguments to encode.
+ * @returns The encoded arguments.
+ */
+function encodeArgs(args: [{ type: string; value: string }]): string {
+  // Instantiate two arrays.
+  const types: string[] = [];
+  const values: string[] = [];
+
+  // Split the array of arguments into arrays of types and values.
+  args.forEach((arg) => {
+    types.push(arg.type);
+    values.push(arg.value);
+  });
+
+  // Encode and array the types and values.
+  const abiCoder = new ethers.utils.AbiCoder();
+  return abiCoder.encode(types, values).replace(/^(0x)/, "");
+}
