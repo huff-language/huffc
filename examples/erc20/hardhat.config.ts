df1cb9ffb76b5bcc8e2cf384a9e7d9de6560c9dd
JetDeveloping <0xhotdog@gmail.com>
2022-03-23 10:56:47 -0700

    Setup Huff testing environment

--- examples/erc20/hardhat.config.ts
+++ examples/erc20/hardhat.config.ts
@@ -1,21 +1,24 @@
-require("@nomiclabs/hardhat-waffle");
+import { task } from "hardhat/config";
+import "@nomiclabs/hardhat-waffle";
+import "hardhat-gas-reporter";
+import "hardhat-huff";
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+task("accounts", "Prints the list of accounts", async (args, hre) => {
   const accounts = await hre.ethers.getSigners();
 
   for (const account of accounts) {
-    console.log(account.address);
+    console.log(await account.address);
   }
 });
 
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
 
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
-module.exports = {
-  solidity: "0.8.4",
+export default {
+  solidity: "0.7.3",
+  huff: {
+    version: "0.0.16",
+  },
 };
