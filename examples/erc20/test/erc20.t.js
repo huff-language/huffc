668a97c06529956087423c97b182ac2180718bad
realdiganta <digantakalita.ai@gmail.com>
2022-04-20 00:04:39 +0530

    added totalSupply & unit tests

--- examples/erc20/test/erc20.t.js
+++ examples/erc20/test/erc20.t.js
@@ -3,10 +3,15 @@ const { ethers } = require("hardhat");
 
 let token;
 let accounts;
+let owner;
+let user;
 
 describe("ERC20", function () {
   beforeEach(async function () {
     accounts = await hre.ethers.getSigners();
+
+    owner = accounts[0].address;
+    user = accounts[1].address;
 
     const Token = await ethers.getContractFactory("ERC20");
     token = await Token.deploy();
@@ -14,15 +19,25 @@ describe("ERC20", function () {
   });
 
   it("Mint & Balance Check", async function () {
-    const user = accounts[0].address;
     let bal = await token.balanceOf(user);
-    console.log("Balance", bal.toNumber());
 
     await token.mint(user, 6900);
-    // await token.setBalance(user, 6900);
 
     bal = await token.balanceOf(user);
 
     expect(bal).to.equal(6900);
+  });
+
+  it("total supply check", async function () {
+    let initSupply = await token.totalSupply();
+
+    expect(initSupply.toNumber()).to.equal(0);
+
+    const val = 6900;
+    await token.mint(user, val);
+
+    let newSupply = await token.totalSupply();
+
+    expect(newSupply).to.equal(val);
   });
 });
