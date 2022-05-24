5af78e6c8d1b3c89a2135d647aecd986f6349826
Patrick Schork <patrick@numer.ai>
2022-05-24 10:13:19 -0700

    Fixes example ERC20 contract deploy script

--- examples/erc20/scripts/deploy.js
+++ examples/erc20/scripts/deploy.js
@@ -14,12 +14,12 @@ async function main() {
   // await hre.run('compile');
 
   // We get the contract to deploy
-  const Greeter = await hre.ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
+  const ERC20 = await hre.ethers.getContractFactory("ERC20");
+  const erc20 = await ERC20.deploy()
 
-  await greeter.deployed();
+  await erc20.deployed();
 
-  console.log("Greeter deployed to:", greeter.address);
+  console.log("ERC20 deployed to:", erc20.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
