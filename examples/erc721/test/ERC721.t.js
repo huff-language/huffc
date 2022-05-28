28c873401945dfc54169f081e5ea70e697571759
exp_table <exp.table@protonmail.com>
2022-05-28 11:59:20 +0200

    ERC721:APPROVE:fixed bug

--- examples/erc721/test/ERC721.t.js
+++ examples/erc721/test/ERC721.t.js
@@ -2,9 +2,14 @@ const { expect } = require("chai");
 const { assert } = require("console");
 const { ethers } = require("hardhat");
 
-let erc721;
+let erc721, owner;
+
+const DEAD = "0x000000000000000000000000000000000000dAeD"
 
 describe("ERC721", function () {
+  before(async function () {
+    [owner] = await ethers.getSigners();
+  });
 
   beforeEach(async function () {
     const ERC721 = await ethers.getContractFactory("ERC721");
@@ -13,14 +18,18 @@ describe("ERC721", function () {
   });
 
   it("ERC721 is deployed", async function () {
-    expect(erc721.address).to.not.equal(
-      "0x0000000000000000000000000000000000000000"
-    );
+    expect(erc721.address).to.not.equal("0x0000000000000000000000000000000000000000");
   });
 
   it("Can mint a NFT", async function () {
-    await erc721.mint("0x000000000000000000000000000000000000dAeD", 0);
-    expect(await erc721.ownerOf(0)).to.equal("0x000000000000000000000000000000000000dAeD");
-    expect(await erc721.balanceOf("0x000000000000000000000000000000000000dAeD")).to.equal(1);
+    await erc721.mint(DEAD, 1337);
+    expect(await erc721.ownerOf(1337)).to.equal(DEAD);
+    expect(await erc721.balanceOf(DEAD)).to.equal(1);
+  });
+
+  it("Can approve a NFT", async function () {
+    await erc721.mint(owner.address, 1337);
+    await erc721.approve(DEAD, 1337);
+    expect(await erc721.getApproved(1337)).to.equal(DEAD);
   });
 });
